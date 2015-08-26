9b3f047123c0e208612b43221906994443027700
katiepeterso <katiepeterso@outlook.com>
2015-08-26 15:51:45 -0700

    Pass Test 2

---
+++ Warcraft 3/Barracks.m
@@ -0,0 +1,20 @@
+//
+//  Barracks.m
+//  
+//
+//  Created by Daniel Mathews on 2015-07-31.
+//
+//
+
+#import "Barracks.h"
+
+@implementation Barracks
+
+-(instancetype)init{
+    _gold = 1000;
+    _food = 80;
+    return self;
+}
+
+
+@end
